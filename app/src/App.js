6105dfa7d028cd0a27e2e24c2685172e58024fa3
RyotArch <bboy.ryu@icloud.com>
2019-05-30 05:34:07 -0700

    Added HOC

--- app/src/App.js
+++ app/src/App.js
@@ -2,25 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function withMousePosition(WrappedComponent) {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { x: 0, y: 0 };
+    }
+
+    componentDidMount() {
+      window.addEventListener("mousemove", this.handleMouseMove);
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener("mousemove", this.handleMouseMove);
+    }
+
+    handleMouseMove = event => {
+      this.setState({
+        x: event.clientX,
+        y: event.clientY
+      });
+    };
+
+    render() {
+      return (
+        <WrappedComponent
+          {...this.props}
+          mousePosition={this.state}
+        />
+      );
+    }
+  };
+}
+function App(props) {
+  const { x, y } = props.mousePosition;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Higher-Order Component Method</h1>
+      <h2>Move the mouse around!</h2>
+      <p style={{ background: "orange" }}>
+        The current mouse position is ({x}, {y})
+      </p>
     </div>
   );
 }
 
-export default App;
+export default withMousePosition(App);
