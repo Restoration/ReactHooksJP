ae4d2c9e90ecab79abe4d7a3da3690d06427b3f1
RyotArch <r.yamamoto.system@gmail.com>
2019-06-04 16:43:53 -0700

    Multiple Reducer
    複数のreducer処理

--- redux-hooks/README.md
+++ redux-hooks/README.md
@@ -2,7 +2,10 @@
 
 
 ## Reducer
+HooksにおけるReducerの動きを
 ReactHooksはReact Functionでのみ使用が可能です。ReduxにおいてReducerを使用する際は標準のJavaScript関数を使用しています。Hooksにおいては`useReducer`を使用します。
+
+
 
 ```javascript
 import React, { useReducer } from 'react';
@@ -34,6 +37,90 @@ const  App = () => {
 
 export default App;
 ```
+
+
+複数の値に対して
+```
+import React, { useReducer } from 'react';
+
+const initialState = {
+  count1: 0,
+  count2: 0,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment1': return { ...state, count1: state.count1 + 1 };
+    case 'decrement1': return { ...state, count1: state.count1 - 1 };
+    case 'set1': return { ...state, count1: action.count };
+    case 'increment2': return { ...state, count2: state.count2 + 1 };
+    case 'decrement2': return { ...state, count2: state.count2 - 1 };
+    case 'set2': return { ...state, count2: action.count };
+    default: throw new Error('Unexpected action');
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <>
+      <div>
+        {state.count1}
+        <button onClick={() => dispatch({ type: 'increment1' })}>+1</button>
+        <button onClick={() => dispatch({ type: 'decrement1' })}>-1</button>
+        <button onClick={() => dispatch({ type: 'set1', count: 0 })}>reset</button>
+      </div>
+      <div>
+        {state.count2}
+        <button onClick={() => dispatch({ type: 'increment2' })}>+1</button>
+        <button onClick={() => dispatch({ type: 'decrement2' })}>-1</button>
+        <button onClick={() => dispatch({ type: 'set2', count: 0 })}>reset</button>
+      </div>
+    </>
+  );
+};
+```
+
+
+上記のコードを分割することも可能ですが、下記のように書いた方が綺麗になります。
+
+```
+import React, { useReducer } from 'react';
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment': return state + 1;
+    case 'decrement': return state - 1;
+    case 'set': return action.count;
+    default: throw new Error('Unexpected action');
+  }
+};
+
+const App = () => {
+  // この場合処理は一緒なのでここでフックさせてる
+  const [count1, dispatch1] = useReducer(reducer, initialState);
+  const [count2, dispatch2] = useReducer(reducer, initialState);
+  return (
+    <>
+      <div>
+        {count1}
+        <button onClick={() => dispatch1({ type: 'increment' })}>+1</button>
+        <button onClick={() => dispatch1({ type: 'decrement' })}>-1</button>
+        <button onClick={() => dispatch1({ type: 'set', count: 0 })}>reset</button>
+      </div>
+      <div>
+        {count2}
+        <button onClick={() => dispatch2({ type: 'increment' })}>+1</button>
+        <button onClick={() => dispatch2({ type: 'decrement' })}>-1</button>
+        <button onClick={() => dispatch2({ type: 'set', count: 0 })}>reset</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
+```
+
 
 
 ## 参考
--- redux-hooks/src/components/App.jsx
+++ redux-hooks/src/components/App.jsx
@@ -1,3 +1,4 @@
+/*
 import * as React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
@@ -12,12 +13,13 @@ const App = () => (
 )
 
 export default App
+*/
 
 
 /*
 useReducer テスト
 
-import React, { useReducer } from 'react';
+import React, { useReducer } from 'react'; // useReducerはHooks内に含まれます
 
 const initialState = 0;
 const reducer = (state, action) => {
@@ -43,3 +45,84 @@ const  App = () => {
 
 export default App;
 */
+
+
+/*
+// 複数の値にたいするReducer
+// 複数を結合したもの
+import React, { useReducer } from 'react';
+
+const initialState = {
+  count1: 0,
+  count2: 0,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment1': return { ...state, count1: state.count1 + 1 };
+    case 'decrement1': return { ...state, count1: state.count1 - 1 };
+    case 'set1': return { ...state, count1: action.count };
+    case 'increment2': return { ...state, count2: state.count2 + 1 };
+    case 'decrement2': return { ...state, count2: state.count2 - 1 };
+    case 'set2': return { ...state, count2: action.count };
+    default: throw new Error('Unexpected action');
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <>
+      <div>
+        {state.count1}
+        <button onClick={() => dispatch({ type: 'increment1' })}>+1</button>
+        <button onClick={() => dispatch({ type: 'decrement1' })}>-1</button>
+        <button onClick={() => dispatch({ type: 'set1', count: 0 })}>reset</button>
+      </div>
+      <div>
+        {state.count2}
+        <button onClick={() => dispatch({ type: 'increment2' })}>+1</button>
+        <button onClick={() => dispatch({ type: 'decrement2' })}>-1</button>
+        <button onClick={() => dispatch({ type: 'set2', count: 0 })}>reset</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
+*/
+
+
+import React, { useReducer } from 'react';
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment': return state + 1;
+    case 'decrement': return state - 1;
+    case 'set': return action.count;
+    default: throw new Error('Unexpected action');
+  }
+};
+
+const App = () => {
+  const [count1, dispatch1] = useReducer(reducer, initialState);
+  const [count2, dispatch2] = useReducer(reducer, initialState);
+  return (
+    <>
+      <div>
+        {count1}
+        <button onClick={() => dispatch1({ type: 'increment' })}>+1</button>
+        <button onClick={() => dispatch1({ type: 'decrement' })}>-1</button>
+        <button onClick={() => dispatch1({ type: 'set', count: 0 })}>reset</button>
+      </div>
+      <div>
+        {count2}
+        <button onClick={() => dispatch2({ type: 'increment' })}>+1</button>
+        <button onClick={() => dispatch2({ type: 'decrement' })}>-1</button>
+        <button onClick={() => dispatch2({ type: 'set', count: 0 })}>reset</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
